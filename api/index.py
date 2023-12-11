29f822f6135c631f45fb8cd2bf1370a8e747c91e
vijayAdam <99938785+vijayasddd@users.noreply.github.com>
2023-12-11 17:12:43 +0800

    d

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
 from googletrans import Translator
 from flask import Flask, request
+from flask_cors import CORS
+
 from collections.abc import Iterable
 import logging
 
 translator = Translator()
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 log = logging.getLogger('werkzeug')
 log.disabled = True
 
